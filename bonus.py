d71629bbc67b17134da04d724a6776cf0abf0660
mishratarini <mishratarini@gmail.com>
2021-09-19 19:03:40 -0400

    Update bonus.py

--- bonus.py
+++ bonus.py
@@ -18,12 +18,13 @@ print(cities.str.contains(r' [A-Z]{2} '))
 # Evaluate Series with match
 print(cities.str.match(r' [A-Z]{2} '))
 
-# Create contactsdf DataFrame from a two-dimensional array.
+# Create contactsdf DataFrame from a two-dimensional array
 contacts = [['Mike Green', 'demo1@deitel.com', '5555555555'], ['Sue Brown', 'demo2@deitel.com', '5555551234']]
 contactsdf = pd.DataFrame(contacts, columns=['Name', 'Email', 'Phone'])
 print(f'contactsdf : \n{contactsdf}')
 
 
+# Format phone numbers from contactsdf DataFrame 
 def get_formatted_ph(value):
     result = re.fullmatch(r'(\d{3})(\d{3})(\d{4})', value)
     return '-'.join(result.groups()) if result else value
